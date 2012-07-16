9fb016d0441978562f2c1b3faa4ea6e72f844f63
Refael Dakar <refael.dakar@capriza.com>
2012-07-16 20:52:16 +0300

    initial commit

---
+++ Soomla-Android-Store/src/com/soomla/store/data/VGood.java
@@ -0,0 +1,14 @@
+package com.soomla.store.data;
+
+public class VGood {
+	private String mName;
+	private String mDescription;
+	
+	public String getName() {
+		return mName;
+	}
+	
+	public String getDescription() {
+		return mDescription;
+	}
+}
