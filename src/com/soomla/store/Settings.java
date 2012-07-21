3d3f78649f31bd0b84466a261bd0446b2b36ed5a
Refael Dakar <refaeldakar@gmail.com>
2012-07-21 10:23:45 +0300

    initial commit.

---
+++ src/com/soomla/store/Settings.java
@@ -0,0 +1,8 @@
+package com.soomla.store;
+
+import android.content.Context;
+
+public class Settings {
+	public static String 	STORE_FILE_PATH = "soomla.store";
+	public static Context 	APPLICATION_CONTEXT = null;
+}
