0881b6c635c17c097ec1e7e26fcc0cd0f80034c1
Refael Dakar <refaeldakar@gmail.com>
2012-08-31 18:43:16 +0300

    constants for the right and easy way to read the JSON

---
+++ SoomlaAndroidStore/src/com/soomla/store/data/JSONConsts.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2012 Soomla Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soomla.store.data;
+
+/**
+ * This class contains all static final String names of the keys in the Store JSON.
+ */
+public class JSONConsts {
+
+    public static final String STORE_TEMPLATE           = "template";
+    public static final String STORE_BACKGROUND         = "background";
+    public static final String STORE_ISCURRENCYDISABLED = "isCurrencyStoreDisabled";
+    public static final String STORE_VIRTUALCURRENCIES  = "virtualCurrencies";
+    public static final String STORE_CURRENCYPACKS      = "currencyPacks";
+    public static final String STORE_VIRTUALGOODS       = "virtualGoods";
+
+    public static final String CURRENCYPACK_PRODUCT_ID  = "productId";
+    public static final String CURRENCYPACK_PRICE       = "price";
+    public static final String CURRENCYPACK_AMOUNT      = "amount";
+    public static final String CURRENCYPACK_CONSUMABLE  = "consumable";
+    public static final String CURRENCYPACK_CURRENCYITEMID = "currency_itemId";
+
+    public static final String GOOD_CURRENCY_VALUE      = "currencyValue";
+
+    public static final String ITEM_NAME                = "name";
+    public static final String ITEM_DESCRIPTION         = "description";
+    public static final String ITEM_IMAGEFILEPATH       = "imgFilePath";
+    public static final String ITEM_ITEMID              = "itemId";
+
+}
