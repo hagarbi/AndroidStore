f042e363fdb7346cd5076f6805d25eeaf54125a4
Refael Dakar <refaeldakar@gmail.com>
2012-09-07 15:33:51 +0300

    auto build stuff

---
+++ buildstuff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+FAILED=0
+cd ./SoomlaAndroidStore
+ant clean
+ant debug
+if [ "$?" = 1 ]; then
+    echo "SoomlaAndroidStore build failed!"
+    FAILED=1
+fi
+cd ..
+
+# cd ./SoomlaAndroidStoreTest
+# ant clean
+# ant test
+# if [ "$?" = 1 ]; then
+	# echo "SoomlaAndroidStoreTest build failed!"
+    # FAILED=1
+# fi
+# cd ..
+exit $FAILED
